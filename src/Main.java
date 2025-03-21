06c9b853b7cbce9ac8dca7672ad1a7fee9705681
Kewen Belmont <141522522+Shellbaka@users.noreply.github.com>
2025-03-21 11:38:44 -0300

    Só uma leve alteração.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         iPhone meuIphone = new iPhone("iPhone 13", "Apple");
         iPhonePro meuIphonePro = new iPhonePro("iPhone 15 Pro", "Apple");
 
-        // Teste do iPhone normal
         System.out.println("Modelo: " + meuIphone.getModelo() + " | Fabricante: " + meuIphone.getFabricante());
         meuIphone.selecionarMusica("Imagine - John Lennon");
         meuIphone.tocar();
@@ -11,7 +10,7 @@ public class Main {
         meuIphone.ligar("11987654321");
         meuIphone.exibirPagina("https://www.google.com");
 
-        // Teste do iPhone Pro
+        
         System.out.println("\nModelo: " + meuIphonePro.getModelo() + " | Fabricante: " + meuIphonePro.getFabricante());
         meuIphonePro.usarFaceID();
     }
